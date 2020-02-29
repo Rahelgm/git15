d45d709eba7bcfd0a2daff75aa5b27f2bc21e540
Rahelgm <raahelaa@gmail.com>
2020-02-29 11:38:09 -0500

    Logout.java

--- src/Test/Logout.java
+++ src/Test/Logout.java
@@ -2,6 +2,8 @@ package Test;
 
 public class Logout {
     //some code
+
+    //updated new code
     public static void main(String[] args) {
         System.out.println(" hello world from logout");
 
